5153622a94af228931fa32e54c2ba58ac816c12a
Silvia Dominguez <silviadmgz@gmail.com>
2024-03-07 16:49:53 +0100

    testing react native

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Linking,
+	Button,
+	Alert,
+	Pressable,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [username, setUsername] = useState('');
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>SIGN IN</Text>
+			<TextInput
+				style={styles.inputField}
+				placeholder='Username'
+				value={username}
+				onChangeText={setUsername}
+			/>
+			<TextInput
+				style={styles.inputField}
+				placeholder='Password'
+				secureTextEntry={true}
+			/>
+
+			<View style={{ flexDirection: 'row', gap: 5 }}>
+				<Text>Don't have an account?</Text>
+				<Text
+					style={styles.registerHyperlink}
+					onPress={() => Linking.openURL('http://google.com')}
+				>
+					Register here!
+				</Text>
+			</View>
+
+			<Pressable
+				onPress={() => Alert.alert('Logging in')}
+				style={styles.loginButton}
+			>
+				<Text style={{ color: 'white', fontSize: 16, padding: 10 }}>LOGIN</Text>
+			</Pressable>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	title: {
+		fontSize: 25,
+		fontWeight: 'bold',
+	},
+	container: {
+		flex: 1,
+		gap: 10,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputField: {
+		padding: 10,
+		height: 30,
+		width: 250,
+		borderColor: 'gray',
+		borderBottomWidth: 1,
+	},
+	registerHyperlink: {
+		color: 'blue',
+	},
+	loginButton: {
+		backgroundColor: 'black',
+		width: 200,
+		alignItems: 'center',
+		marginTop: 30,
+	},
 });
